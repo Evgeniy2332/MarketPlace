879ae3fdd1ff1f11697a09c86782f1d8b9b988e1
Evgeniy2332 <pob32925@yandex.ru>
2024-01-29 21:48:44 +0300

    Initial commit

--- src/main/java/com/example/Sale/SaleApplication.java
+++ src/main/java/com/example/Sale/SaleApplication.java
@@ -3,9 +3,11 @@ package com.example.Sale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.Sale")
+@EnableJpaRepositories(basePackages = "com.example.Sale")
 public class SaleApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/Sale/repositories/UserRepository.java
+++ src/main/java/com/example/Sale/repositories/UserRepository.java
@@ -4,7 +4,7 @@ import com.example.Sale.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
